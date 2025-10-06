6f31d3d5c48408c968ea48ea2249722bed13dde0
christinawdc <christinawdc@gmail.com>
2025-10-07 01:10:40 +0530

    added copy to clipboard feature

--- password-generator.py
+++ password-generator.py
@@ -1,8 +1,12 @@
-import random, string
+import random, string, pyperclip
 
 length = int(input("Enter password length: "))
 
 chars = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(random.choice(chars) for _ in range(length))
+
+# to copy password to clipboard
+pyperclip.copy(password)
+print("Password copied to clipboard!")
 
 print("Generated password:", password)
